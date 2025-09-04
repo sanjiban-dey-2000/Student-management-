beded4c9caf509adc5ddfcabdd02ec1e036c9b97
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-09-04 13:10:50 +0530

    Course repository created

---
+++ src/main/java/com/student/studentmanagement/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.student.studentmanagement.repository;
+
+import com.student.studentmanagement.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course,Long> {
+}
