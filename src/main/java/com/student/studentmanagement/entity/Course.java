bcf24aef6bffa3ddd31947af4a9a0d264fe93cc1
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-09-04 13:07:38 +0530

    Course entity created

---
+++ src/main/java/com/student/studentmanagement/entity/Course.java
@@ -0,0 +1,32 @@
+package com.student.studentmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int duration;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column(nullable = false)
+    private String description;
+
+}
