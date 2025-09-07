849c0171b4f97947196da56dda6416e922536fd8
Sanjiban Dey <sanjibandey2000@gmail.com>
2025-09-07 10:09:47 +0530

    Course Dto created

--- src/main/java/com/student/studentmanagement/dto/CourseDto.java
+++ src/main/java/com/student/studentmanagement/dto/CourseDto.java
@@ -1,4 +1,16 @@
 package com.student.studentmanagement.dto;
 
+import lombok.*;
+
+@Data
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CourseDto {
+    private Long id;
+    private String name;
+    private int duration;
+    private double price;
+    private String description;
 }
